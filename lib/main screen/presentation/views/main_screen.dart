481d87d6d4c62b06b2e0a508a421cd404d4329e5
esraaehab333 <esraaehabb333@gmail.com>
2025-02-13 00:13:28 +0200

    Remove unnecessary blank line in MainScreen widget

--- lib/main screen/presentation/views/main_screen.dart
+++ lib/main screen/presentation/views/main_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
