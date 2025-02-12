8bab01df919987c77a7b567c070da94ba3448bc9
esraaehab333 <esraaehabb333@gmail.com>
2025-02-12 23:01:36 +0200

    Add initial implementation of main screen and related components

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const myBink = Color(0xFFF7CFD8);
+  static const myBlack = Colors.black;
+  static const myGrey = Color(0xFF6F6F6F);
+  static const myGrey_1 = Color(0xffDADADA);
+  static const myWhite = Colors.white;
+  // static const myBlue = Color(0xFF73C7C7);
+  static const formColor = Color(0xFFF6F6F6);
+  static const myBlue = Color(0xFF008080);
+}
---
+++ lib/core/app_images.dart
@@ -0,0 +1,13 @@
+class Assets {
+  static const String assetsImagesLogoPng = 'assets/images/logo.png';
+  static const String assetsImagesOnboarding1Png =
+      'assets/images/onboarding1.png';
+  static const String assetsImagesOnboarding2Png =
+      'assets/images/onboarding2.png';
+  static const String assetsImagesOnboarding3Png =
+      'assets/images/onboarding3.png';
+  static const String assetsImagesOnboarding1gif =
+      'assets/images/onboardingone.gif';
+  static const String assetsImagesOnboarding2gif =
+      'assets/images/onboardingtwo.gif';
+}
